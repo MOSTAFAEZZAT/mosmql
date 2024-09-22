a9babfe50350be6f52a9ffe5ace7d6aa01a6e831
MOSTAFAEZZAT <mostafaezzat318@gmail.com>
2024-09-22 07:31:22 -0500

    Updating the whole funcationality to class

--- index.js
+++ index.js
@@ -3,84 +3,86 @@ const dotenv = require('dotenv').config();
 const fs = require('fs');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+class Mosmql {
+    constructor(openaiApiKey, filePath, database, mongodbUri) {
+        this.openaiApiKey = openaiApiKey;
+        this.filePath = filePath;
+        this.database = database;
+        this.mongodbUri = mongodbUri;
+        this.openai = new OpenAI({ apiKey: openaiApiKey });
+    }
 
-async function executeMongodbQuery(query, database, mongodbUri) {
-    try {
+    async executeMongodbQuery(query) {
+        try {
+            // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+            var client = new MongoClient(this.mongodbUri, {
+                serverApi: {
+                    version: ServerApiVersion.v1,
+                    strict: true,
+                    deprecationErrors: true,
+                }
+            });
+            // Connect the client to the server (optional starting in v4.7)
+            await client.connect();
+            // Send a ping to confirm a successful connection
+            await client.db("admin").command({ ping: 1 });
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+            const db = client.db(this.database);
+            // Process query
+            query = query.toString().replace(/\\/g, '');
+            var dbResult = eval(query).then(res => { return res; });
+            var dbResult = await this.databaseDataResult(dbResult);
+            return JSON.stringify(dbResult);
 
-        // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-        var client = new MongoClient(mongodbUri, {
-            serverApi: {
-                version: ServerApiVersion.v1,
-                strict: true,
-                deprecationErrors: true,
-            }
+        } catch (err) {
+            console.log(err);
+        } finally {
+            // Ensures that the client will close when you finish/error
+            await client.close();
+        }
+    }
+
+    async databaseDataResult(promise) {
+        const data = await promise;
+        return data;
+    }
+
+    async mosmql(query) {
+        const dbQuery = await this.openaiCompletion(query);
+        const mongodbResult = await this.executeMongodbQuery(dbQuery);
+        return mongodbResult;
+    }
+
+    async openaiCompletion(query) {
+        var data = await fs.promises.readFile(this.filePath, 'utf8');
+        const learningModel = data;
+        var mqlQueryAi = learningModel + "Q:" + query + "\nA:";
+
+        const completion = await this.openai.chat.completions.create({
+            messages: [
+                { "role": "system", "content": "You are a Database system machine"},
+                { "role": "user", "content": mqlQueryAi },
+            ],
+            model: "gpt-3.5-turbo",
+            max_tokens: 100,
+            temperature: 0,
         });
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-        const db = client.db(database);
-        // query sample
-        // var query = 'db.collection("restaurants").find({"grades.score": {$gt: 10}})';
-        query = query.toString().replace(/\\/g, '');
-        // uncomment to debug
-        // console.log("query" + query);
-        var dbResult = eval(query).then(res => { return res; });
-        var dbResult = await databaseDataResult(dbResult);
-        return JSON.stringify(dbResult);
 
-    } catch (err) {
-        console.log(err);
-    }
-    finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+        console.log(completion.choices[0]);
+        const dbQuery = completion.choices[0].message.content;
+        return dbQuery;
     }
 }
 
-async function databaseDataResult(promise) {
-    const data = await promise;
-    return data;
-}
-
-
-async function mosmql(query, openaiApiKey, filePath, database, mongodbUri) {
-
-    const dbQuery = await openaiCompletion(query, openaiApiKey, filePath);
-    const mongodbResult = await executeMongodbQuery(dbQuery, database, mongodbUri);
-    // in case of debugging uncomment
-    // console.log(mongodbResult);
-    return mongodbResult;
-}
-
-async function openaiCompletion(query, openaiApiKey, filePath) {
-
-
-    const openai = new OpenAI({ apiKey: openaiApiKey });
-
-    var data = await fs.promises.readFile(filePath, 'utf8');
-
-    const learningModel = data;
-
-    var mqlQueryAi = learningModel + "Q:" + query + "\nA:";
-    const completion = await openai.completions.create({
-        model: "text-davinci-003",
-        prompt: mqlQueryAi,
-        max_tokens: 100,
-        temperature: 0,
-    });
-    const dbQuery = completion.choices[0].text;
-    // uncomment when you need debug
-    // console.log("dbQuery" + dbQuery + "\n\n");
-
-    return dbQuery;
-}
-
-//  Sample call
+// Sample invoke
 /* (async () => {
-    console.log(await mosmql("Query restaurants collection for restaurants with Bakery cuisine", process.env.OPENAI_API_KEY,
-        './models/trainingModels/TrainingModels.txt', "sample_restaurants", process.env.uri))
+    const mosmqlInstance = new Mosmql(
+        process.env.OPENAI_API_KEY,
+        './models/trainingModels/TrainingModels.txt',
+        "sample_restaurants",
+        process.env.uri
+    );
+    console.log(await mosmqlInstance.mosmql("Query restaurants collection for restaurants with Bakery cuisine"));
 })(); */
 
-module.exports = { mosmql };
+module.exports = Mosmql;
